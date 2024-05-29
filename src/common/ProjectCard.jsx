092605cdaab5ed5820824648137f6a9ac90aff95
SzKriszti <krisztinaszabo98@gmail.com>
2024-05-29 16:33:15 +0200

    2 projects pictures added

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,7 +1,7 @@
 function ProjectCard({link, picture, title, decription}) {
   return (
     <a href={link} target="_blank">
-      <img src={picture} alt={`${title} picture`}></img>
+      <img src={picture} alt={`${title} picture`} className="projectPictures"></img>
       <h4>{title}</h4>
       <p>{decription}</p>
     </a>
--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -1,5 +1,8 @@
 import style from './ProjectStyle.module.css'
 import ProjectCard from '../../common/ProjectCard'
+import countriesPicture from '../../assets/countries.png'
+import RickMortyPicture from '../../assets/rickandmorty.png'
+
 
 function Projects() {
   return (
@@ -8,18 +11,20 @@ function Projects() {
       <div className={style.projectsList}>
         <ProjectCard 
           link='https://github.com/SzKriszti/countries' 
+          picture={countriesPicture}
           title='Countries' 
           decription='In this webpage the user could search for countries by their names, sort them ascending or descending by population, or read other detailt, by clicking the show more button. This project was made with Vite + React.js and I used MUI for the UI design.'
         />
         <ProjectCard
           link='https://github.com/SzKriszti/SzKriszti_Rick-Morty_webpage'
+          picture={RickMortyPicture}
           title='Rick and Morty webpage'
           decription='In this webpage the user could read some details about the caracters. I used JavaScript, API, HTML, CSS, SCSS in this project.'
         />
 
 
 
-        
+
       </div>
     </section>
   )
